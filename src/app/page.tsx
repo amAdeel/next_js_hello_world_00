0ec652f6a6313b78b850c981445d038edc8b0ab8
amAdeel <152018163+amAdeel@users.noreply.github.com>
2024-03-19 16:37:27 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+function HomePage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl text-blue-600 mb-4">اَلسَّلَامُ عَلَیْکُمْ</h1>
+      <p className="text-center text-gray-800">Hello, I am Adeel and this is my first Next.js project.</p>
+    </div>
+  );
+}
+
+export default HomePage;
